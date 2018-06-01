35a53f65a23fd4a15e0e56e7958e3255f8af506a
rusleysantos <rusley_silva@hotmail.com>
2018-06-01 11:40:32 -0300

    Add files via upload

--- js/meujs.js
+++ js/meujs.js
@@ -1,24 +1,23 @@
-var Tempo=0, Valor=0, Taxa=0, TaxaR=0, i=0, Resultado=0, cont=0,re;
+var Tempo = 0,
+    Valor = 0,
+    Taxa = 0,
+    TaxaR = 0,
+    i = 0,
+    Resultado = 0,
+    Cont = 0,
+    Arre;
 
-function Calculo() 
-{
-
+function Calculo() {
 
     Tempo = parseInt(document.getElementById("tempo").value);
     Valor = parseInt(document.getElementById("valor").value);
     Taxa = parseInt(document.getElementById("taxa").value);
     TaxaR = Taxa / 100;
-    //Resultado = Math.pow((Valor * (1 + TaxaR)), Tempo);
 
-   // for (i = 0; i < Tempo; i++) 
-   // {
-       // cont++;
-        re = Valor * (1 + TaxaR);
-        Resultado = re^2;
-        
-
-  //  }
-
-    debugger;
-
+    for (i = 0; i < Tempo; i++) {
+        Cont++;
+        Resultado = Math.pow(Valor * (1 + (Taxa / 100)), Cont);
+        Arre = parseFloat(Resultado.toFixed(2));
+        alert("No mês " + Cont + " sua divida e de R$" + Arre)
+    }
 }
